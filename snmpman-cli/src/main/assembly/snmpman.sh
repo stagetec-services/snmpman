bacedf38526049bce728809dc6debf930faf8afd
Michael Bachmann <m.bachmann@stagetec-services.com>
2024-08-23 15:52:48 +0200

    bnd-ified project

--- snmpman-cli/src/main/assembly/snmpman.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-BASEDIR=$(cd $(dirname "$0")/..; pwd)
-JAR=${BASEDIR}/lib/snmpman-cli-${project.version}.jar
-if [ "$LOGXML" = "" ]; then
-  export LOGXML=file:///${BASEDIR}/etc/log4j2.xml
-fi
-
-java -Dlog4j.configurationFile=${LOGXML} -jar ${JAR} "$@"
